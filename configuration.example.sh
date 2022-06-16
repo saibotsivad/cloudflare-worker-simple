96326ecb5b136032affb6002a3227f85af95e5cd
Tobias Davis <tobias@davistobias.com>
2022-06-16 14:56:11 -0500

    change: update configs to have more details

--- configuration.example.sh
+++ configuration.example.sh
@@ -6,4 +6,6 @@
 #
 # Go here and generate a token, if you don't have one already: https://dash.cloudflare.com/profile/api-tokens
 #
-export CF_API_TOKEN=""
+export CF_API_TOKEN="veryLongAPIToken"
+export CF_ACCOUNT_ID="123"
+export CF_ZONE_ID="456"
